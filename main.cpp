82926329886c8bdce5d249e6306de046ffc26e27
vngupro <60599812+vngupro@users.noreply.github.com>
2026-01-17 19:04:14 -0500

    init glad

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+
+#include <glad/glad.h>
+
+using namespace std;
+
+int main(int argc, char* argv[], char* envp[])
+{
+	cout << "Program Name: " << argv[0] << endl;
+	cout << "Arguments:" << endl;
+	for (int i = 1; i < argc; ++i) {
+		cout << "  argv[" << i << "]: " << argv[i] << endl;
+	}
+	cout << "Environment Variables:" << endl;
+	for (char** env = envp; *env != nullptr; ++env) {
+		cout << "  " << *env << endl;
+	}
+	return 0;
+}
