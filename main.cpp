b058598da9eb2d8ae780a3511ac9dd398b2db689
vngupro <60599812+vngupro@users.noreply.github.com>
2026-01-28 20:55:34 -0500

    image

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
+
+#include "stb_image.h"
 
 #include "Mesh.h"
 #include "Rectangle.h"
@@ -138,6 +137,28 @@ int main()
 	Circle circle(0.5f, 64);
 	ScreenQuad screenQuad;
 
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    // set the texture wrapping/filtering options (on the currently bound texture object)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load and generate the texture
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
@@ -158,6 +179,9 @@ int main()
 
         // set the current animation type from the cycling key
         shader.setInt("uAnimType", currentAnimType);
+
+        int width, height, nrChannels;
+        unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
 
         switch (currentMesh)
         {
