9bdaa2ee081cffe918b8e2168b69e5aaff4e0d8f
vngupro <60599812+vngupro@users.noreply.github.com>
2026-02-24 20:42:18 -0500

    macro

--- mygl_loader.h
+++ mygl_loader.h
@@ -4,3 +4,28 @@
 #error OpenGL header already included, remove this include, glad already provides it
 #endif
 #define __gl_h_
+
+#if defined(_WIN32) && !defined(APIENTRY) && !defined(__CYGWIN__) && !defined(__SCITECH_SNAP__)
+#define APIENTRY __stdcall
+#endif
+
+#ifndef APIENTRY
+#define APIENTRY
+#endif
+
+#ifndef APIENTRYP
+#define APIENTRYP APIENTRY *
+#endif
+
+#ifndef MYGLAPIENTRY
+#define MYGLAPIENTRY APIENTRY
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#ifdef __cplusplus
+}
+#endif
