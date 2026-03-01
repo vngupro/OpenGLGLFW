830385b586664a3b66189a62f8402efd1ff55625
vngupro <60599812+vngupro@users.noreply.github.com>
2026-02-28 21:00:31 -0500

    basic typedef

--- mygl_loader.hpp
+++ mygl_loader.hpp
@@ -19,3 +19,13 @@ GLAPI struct myGLversionStruct GLVersion;
 
 GLAPI int gladLoadGL(void);
 GLAPI int gladLoadGLLoader(MYGLloadproc);
+
+#include <cstdint>
+
+using GLint = std::int32_t;
+using GLuint = std::uint32_t;
+using GLfloat = float;
+using GLdouble = double;
+using GLboolean = std::uint8_t;
+using GLsizeiptr = std::intptr_t;
+using GLchar = char;
