a323a26d993acddda5828b64d6d7becd55dec17c
vngupro <60599812+vngupro@users.noreply.github.com>
2026-03-26 21:42:40 -0400

    more

--- mygl_loader.hpp
+++ mygl_loader.hpp
@@ -187,3 +187,9 @@ inline PFNGLVIEWPORTPROC glViewport = nullptr;
 #define GL_BLEND                          0x0BE2
 #define GL_SRC_ALPHA                      0x0302
 #define GL_ONE_MINUS_SRC_ALPHA            0x0303
+
+using PFNGLDRAWARRAYSPROC = void (*)(GLenum mode, GLint first, GLsizei count);
+using PFNGLDRAWELEMENTSPROC = void (*)(GLenum mode, GLsizei count, GLenum type, const void* indices);
+
+inline PFNGLDRAWARRAYSPROC glDrawArrays = nullptr;
+inline PFNGLDRAWELEMENTSPROC glDrawElements = nullptr;
