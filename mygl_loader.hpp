3483d6fce1ef044129f22d8f6605b305ea2ced38
vngupro <60599812+vngupro@users.noreply.github.com>
2026-03-28 20:25:01 -0400

    update

--- mygl_loader.hpp
+++ mygl_loader.hpp
@@ -193,3 +193,5 @@ inline PFNGLDRAWELEMENTSPROC glDrawElements = nullptr;
 #define GL_BLEND                          0x0BE2
 #define GL_SRC_ALPHA                      0x0302
 #define GL_ONE_MINUS_SRC_ALPHA            0x0303
+
+// toclean
